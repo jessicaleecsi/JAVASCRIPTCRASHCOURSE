709aa27921327ca8aa1bb5586b12a6df6297649b
Jessica Farley <jessicaleecsi@yahoo.com>
2026-05-11 21:30:16 -0400

    no changes

--- app.js
+++ app.js
@@ -72,10 +72,6 @@ function isLoggedInOrSubscribed (loggedIn, subscribed) {
         
 }
 console.log (isLoggedInOrSubscribed ('LOGGED_OUT', 'NOT_SUBSCRIBED'))
-
-//* MEDIUM SECTION *//
-
-
 function falsyOrTruthy (elem1, elem2) {
     return !elem1 ? elem1 : elem2;
 }
